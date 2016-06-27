37bb207cc4a06bd6508b45e752fe910b7d02ef43
FELIPE <FELIPE@DESKTOP-NU2QPN0>
2016-06-27 17:42:49 -0300

    inicio de sesion

--- Casino/login.Designer.vb
+++ Casino/login.Designer.vb
@@ -22,97 +22,133 @@ Partial Class login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.txtusuario = New System.Windows.Forms.TextBox()
-        Me.txtclave = New System.Windows.Forms.TextBox()
-        Me.lblusuario = New System.Windows.Forms.Label()
-        Me.lblcontraseña = New System.Windows.Forms.Label()
-        Me.btnentrar = New System.Windows.Forms.Button()
-        Me.btnsalir = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btniniciarsesion = New System.Windows.Forms.Button()
+        Me.txtingresesupassword = New System.Windows.Forms.TextBox()
+        Me.txtingresesuusuario = New System.Windows.Forms.TextBox()
+        Me.lbliniciarsesion = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'txtusuario
+        'Panel1
         '
-        Me.txtusuario.Location = New System.Drawing.Point(136, 65)
-        Me.txtusuario.Name = "txtusuario"
-        Me.txtusuario.Size = New System.Drawing.Size(100, 20)
-        Me.txtusuario.TabIndex = 0
+        Me.Panel1.BackColor = System.Drawing.Color.LightGray
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.btniniciarsesion)
+        Me.Panel1.Controls.Add(Me.txtingresesupassword)
+        Me.Panel1.Controls.Add(Me.txtingresesuusuario)
+        Me.Panel1.Location = New System.Drawing.Point(12, 109)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(356, 217)
+        Me.Panel1.TabIndex = 0
+        Me.Panel1.Visible = False
         '
-        'txtclave
+        'btniniciarsesion
         '
-        Me.txtclave.Location = New System.Drawing.Point(136, 117)
-        Me.txtclave.Name = "txtclave"
-        Me.txtclave.Size = New System.Drawing.Size(100, 20)
-        Me.txtclave.TabIndex = 1
+        Me.btniniciarsesion.BackColor = System.Drawing.Color.Black
+        Me.btniniciarsesion.Cursor = System.Windows.Forms.Cursors.AppStarting
+        Me.btniniciarsesion.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btniniciarsesion.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btniniciarsesion.Location = New System.Drawing.Point(16, 149)
+        Me.btniniciarsesion.Name = "btniniciarsesion"
+        Me.btniniciarsesion.Size = New System.Drawing.Size(327, 37)
+        Me.btniniciarsesion.TabIndex = 7
+        Me.btniniciarsesion.Text = "Iniciar sesion"
+        Me.btniniciarsesion.UseVisualStyleBackColor = False
         '
-        'lblusuario
+        'txtingresesupassword
         '
-        Me.lblusuario.AutoSize = True
-        Me.lblusuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblusuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.lblusuario.Location = New System.Drawing.Point(43, 64)
-        Me.lblusuario.Name = "lblusuario"
-        Me.lblusuario.Size = New System.Drawing.Size(60, 18)
-        Me.lblusuario.TabIndex = 2
-        Me.lblusuario.Text = "Usuario"
+        Me.txtingresesupassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtingresesupassword.Location = New System.Drawing.Point(16, 83)
+        Me.txtingresesupassword.Name = "txtingresesupassword"
+        Me.txtingresesupassword.Size = New System.Drawing.Size(327, 26)
+        Me.txtingresesupassword.TabIndex = 6
+        Me.txtingresesupassword.Text = "Ingrese Su Password"
         '
-        'lblcontraseña
+        'txtingresesuusuario
         '
-        Me.lblcontraseña.AutoSize = True
-        Me.lblcontraseña.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblcontraseña.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.lblcontraseña.Location = New System.Drawing.Point(31, 119)
-        Me.lblcontraseña.Name = "lblcontraseña"
-        Me.lblcontraseña.Size = New System.Drawing.Size(85, 18)
-        Me.lblcontraseña.TabIndex = 3
-        Me.lblcontraseña.Text = "Contraseña"
+        Me.txtingresesuusuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtingresesuusuario.Location = New System.Drawing.Point(16, 12)
+        Me.txtingresesuusuario.Name = "txtingresesuusuario"
+        Me.txtingresesuusuario.Size = New System.Drawing.Size(327, 26)
+        Me.txtingresesuusuario.TabIndex = 5
+        Me.txtingresesuusuario.Text = "Ingrese  Su Usuario"
         '
-        'btnentrar
+        'lbliniciarsesion
         '
-        Me.btnentrar.BackColor = System.Drawing.Color.Lime
-        Me.btnentrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnentrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.btnentrar.Location = New System.Drawing.Point(60, 212)
-        Me.btnentrar.Name = "btnentrar"
-        Me.btnentrar.Size = New System.Drawing.Size(84, 37)
-        Me.btnentrar.TabIndex = 4
-        Me.btnentrar.Text = "Entrar "
-        Me.btnentrar.UseVisualStyleBackColor = False
+        Me.lbliniciarsesion.AutoSize = True
+        Me.lbliniciarsesion.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbliniciarsesion.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lbliniciarsesion.Location = New System.Drawing.Point(164, 41)
+        Me.lbliniciarsesion.Name = "lbliniciarsesion"
+        Me.lbliniciarsesion.Size = New System.Drawing.Size(191, 33)
+        Me.lbliniciarsesion.TabIndex = 1
+        Me.lbliniciarsesion.Text = "Iniciar Sesion"
         '
-        'btnsalir
+        'PictureBox1
         '
-        Me.btnsalir.BackColor = System.Drawing.Color.Red
-        Me.btnsalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnsalir.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.btnsalir.Location = New System.Drawing.Point(165, 211)
-        Me.btnsalir.Name = "btnsalir"
-        Me.btnsalir.Size = New System.Drawing.Size(89, 38)
-        Me.btnsalir.TabIndex = 5
-        Me.btnsalir.Text = "Salir"
-        Me.btnsalir.UseVisualStyleBackColor = False
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.Location = New System.Drawing.Point(316, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(27, 26)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.White
+        Me.PictureBox2.Location = New System.Drawing.Point(316, 83)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(27, 26)
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.White
+        Me.PictureBox3.Location = New System.Drawing.Point(75, 12)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(83, 78)
+        Me.PictureBox3.TabIndex = 9
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Visible = False
         '
         'login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.ClientSize = New System.Drawing.Size(284, 261)
-        Me.Controls.Add(Me.btnsalir)
-        Me.Controls.Add(Me.btnentrar)
-        Me.Controls.Add(Me.lblcontraseña)
-        Me.Controls.Add(Me.lblusuario)
-        Me.Controls.Add(Me.txtclave)
-        Me.Controls.Add(Me.txtusuario)
+        Me.ClientSize = New System.Drawing.Size(385, 350)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.lbliniciarsesion)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "login"
         Me.Text = "login"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents txtusuario As TextBox
-    Friend WithEvents txtclave As TextBox
-    Friend WithEvents lblusuario As Label
-    Friend WithEvents lblcontraseña As Label
-    Friend WithEvents btnentrar As Button
-    Friend WithEvents btnsalir As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btniniciarsesion As Button
+    Friend WithEvents txtingresesupassword As TextBox
+    Friend WithEvents txtingresesuusuario As TextBox
+    Friend WithEvents lbliniciarsesion As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
 End Class
--- Casino/login.vb
+++ Casino/login.vb
@@ -1,3 +1,9 @@
 ﻿Public Class login
+    Private Sub login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
 End Class
